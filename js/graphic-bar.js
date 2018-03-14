5a132a20a86897e2abf0cc17d601a016a007342f
ciberpiranha <ciberpiranha@gmail.com>
2018-03-14 16:48:28 +0100

    adding new jobs and spinner

--- js/graphic-bar.js
+++ js/graphic-bar.js
@@ -53,60 +53,7 @@ var data = [
 {week: 'Semana 52',month:"diciembre",Resto:7.61,Bankia:2.69,CaixaBank:20.03,Santander:4.27,BBVA:2.77,ING:62.63},
 ];
 
-var dataLine= [
-{week: 'Semana 1',month:"enero",total:6797580.31},
-{week: 'Semana 2',month:"enero",total:9385745.71},
-{week: 'Semana 3',month:"enero",total:12474134.20},
-{week: 'Semana 4',month:"enero",total:11604372.27},
-{week: 'Semana 5',month:"febrero",total:9524675.83},
-{week: 'Semana 6',month:"febrero",total:11510681.82},
-{week: 'Semana 7',month:"febrero",total:17448712.23},
-{week: 'Semana 8',month:"febrero",total:12291860.05},
-{week: 'Semana 9',month:"marzo",total:17258005.50},
-{week: 'Semana 10',month:"marzo",total:14074585.07},
-{week: 'Semana 11',month:"marzo",total:10773836.43},
-{week: 'Semana 12',month:"marzo",total:9790242.76},
-{week: 'Semana 13',month:"abril",total:8302301.47},
-{week: 'Semana 14',month:"abril",total:6805755.48},
-{week: 'Semana 15',month:"abril",total:2796408.83},
-{week: 'Semana 16',month:"abril",total:5389803.27},
-{week: 'Semana 17',month:"abril",total:7874928.68},
-{week: 'Semana 18',month:"mayo",total:9027273.30},
-{week: 'Semana 19',month:"mayo",total:13438836.18},
-{week: 'Semana 20',month:"mayo",total:12862035.21},
-{week: 'Semana 21',month:"mayo",total:14001664.25},
-{week: 'Semana 22',month:"junio",total:10267089.92},
-{week: 'Semana 23',month:"junio",total:10537409.43},
-{week: 'Semana 24',month:"junio",total:15095536.75},
-{week: 'Semana 25',month:"junio",total:19953532.95},
-{week: 'Semana 26',month:"julio",total:11571210.77},
-{week: 'Semana 27',month:"julio",total:12624911.71},
-{week: 'Semana 28',month:"julio",total:8611103.84},
-{week: 'Semana 29',month:"julio",total:6476242.01},
-{week: 'Semana 30',month:"julio",total:10003579.45},
-{week: 'Semana 31',month:"agosto",total:7133941.57},
-{week: 'Semana 32',month:"agosto",total:7992528.70},
-{week: 'Semana 33',month:"agosto",total:6290937.14},
-{week: 'Semana 34',month:"agosto",total:7498056.09},
-{week: 'Semana 35',month:"septiembre",total:12152122.17},
-{week: 'Semana 36',month:"septiembre",total:9606325.50},
-{week: 'Semana 37',month:"septiembre",total:10535749.36},
-{week: 'Semana 38',month:"septiembre",total:13067549.61},
-{week: 'Semana 39',month:"octubre",total:15798996.85},
-{week: 'Semana 40',month:"octubre",total:18471142.16},
-{week: 'Semana 41',month:"octubre",total:16457513.02},
-{week: 'Semana 42',month:"octubre",total:13282888.06},
-{week: 'Semana 43',month:"octubre",total:12029552.46},
-{week: 'Semana 44',month:"noviembre",total:11324425.23},
-{week: 'Semana 45',month:"noviembre",total:8285594.60},
-{week: 'Semana 46',month:"noviembre",total:7145376.96},
-{week: 'Semana 47',month:"noviembre",total:16660421.68},
-{week: 'Semana 48',month:"diciembre",total:32943982.62},
-{week: 'Semana 49',month:"diciembre",total:12001507.24},
-{week: 'Semana 50',month:"diciembre",total:19947660.94},
-{week: 'Semana 51',month:"diciembre",total:20672806.02},
-{week: 'Semana 52',month:"diciembre",total:14202458.56},
-]
+
 var stackColumnWidth = 12
 var xData = ["Resto","Bankia","CaixaBank","Santander","BBVA","ING"];
 var clientWidth = document.getElementById('stackedchart').clientWidth
@@ -210,10 +157,6 @@ var months_week_thicks = [1,6,10,15,20,25,29,33,37,41,45,49];
     .attr("class", "axis")
     .attr("transform", "translate(0," + (height/1.85) + ")")
     .call(xAxis);
-
-
-
-//svg.append("path").attr("d", averageline(dataLine));// Add the valueline path.
 
 
 
