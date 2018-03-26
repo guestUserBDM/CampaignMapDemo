f7d37258086ed034c9b2810e31f0baa6a3abf6fc
ciberpiranha <ciberpiranha@gmail.com>
2018-03-26 12:17:54 +0200

    adding datepicker by weeks

--- js/application.js
+++ js/application.js
@@ -19,14 +19,7 @@ $( document ).ready(function() {
         'Authorization': 'Token token=44d7817dc75942288e8b36425cfbdea12'},
       })
       .done(function( data ) {
-        arrayDataLinearChart = $.prepareDataForLinearChart(data);
-        dataBarChart = $.prepareDataForBarChart(arrayDataLinearChart,data);
-        dataTotalInvestment = $.getFullYearInvestment(arrayDataLinearChart);
-        dataHeatMap = $.prepareDataForHeatMap(dataTotalInvestment,data);
-        $.drawLineChart(arrayDataLinearChart);
-        $.drawBarChart(dataBarChart);
-        $.drawHeatMap(dataHeatMap);
-        $.drawLegend();
+        $.renderFullPage(data);
         $('button.handCursor').removeClass('hidden_campaign')
     })
       .fail(function(data) {
@@ -34,10 +27,46 @@ $( document ).ready(function() {
     });
 //END Ajax Calls
 
+// -- define your functions
+//This function receive the data response and render all graphics
+$.renderFullPage = function(data){
+	arrayDataLinearChart = $.prepareDataForLinearChart(data);
+    dataBarChart = $.prepareDataForBarChart(arrayDataLinearChart,data);
+    dataTotalInvestment = $.getFullYearInvestment(arrayDataLinearChart);
+    dataHeatMap = $.prepareDataForHeatMap(dataTotalInvestment,data);
+    $.drawLineChart(arrayDataLinearChart);
+    $.drawBarChart(dataBarChart);
+    $.drawHeatMap(dataHeatMap);
+    $.drawLegend();
+}//END FUNCTION: $.renderFullPage()
+
+// This funtions returns the full year investment
 $.getFullYearInvestment = function(totalArray) {
 	sum = 0
 	$.each(totalArray,function(){sum+= this || 0;});
 	return sum
-};
+};//END FUNCTION: $.getFullYearInvestment()
+
+//DOM ELEMENTS EVENTS
+	$('input[name="daterange"]').daterangepicker({
+        "showWeekNumbers": true,
+        "autoApply": true,
+        //"parentEl": ".customDatepickerWidth",
+        "linkedCalendars": false,
+        "showCustomRangeLabel": false,
+        "startDate": "01/01/2017",
+        "endDate": "31/12/2017",
+        "opens": "center",
+        "locale": { 
+          "weekLabel": "S"
+         }
+    }, function(start, end, label) {
+      console.log('New date range selected: ' + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    });
+	$('body').on('click','.list-group-item', function() {
+          $('.oi', this)
+            .toggleClass('oi-caret-right')
+            .toggleClass('oi-caret-bottom');
+        });
 //END doc READY
 });
